b9ffda10fc8c67ad4cf62ab156b6d7e5fa530fce
Alexandru Mincu <alexandrumincu237@gmail.com>
2023-08-01 16:31:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstRepositoryProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
