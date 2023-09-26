cccb7c624d93bb8fa1f5e8ce7b3eadd4d91e8a4a
Alexandru Mincu <alexandrumincu237@gmail.com>
2023-09-26 15:05:51 +0300

    Added a few methods inside the Ranking class. Still incomplete.

--- FootballRanking/FootballRanking/Ranking.cs
+++ FootballRanking/FootballRanking/Ranking.cs
@@ -9,10 +9,37 @@ namespace FootballRanking
     public class Ranking
     {
         private Team[] teams;
+        private int numTeam = 0;
 
-        public Ranking(Team[] teams)
+        public void AddTeam(Team team)
         {
-            this.teams = teams;
+            teams[numTeam++] = team;
+        }
+
+        public int ReportPositionOfTeam(string teamName)
+        {
+            
+        }
+
+        public void UpdateRanking(string winningTeam, string losingTeam)
+        {
+            
+        }
+
+        private void SorteazaEchipeDupaPunctaj()
+        {
+            for (int i = 0; i < numTeam - 1; i++)
+            {
+                for (int j = i + 1; j < numTeam; j++)
+                {
+                    if (teams[i]. < teams[j].Punctaj)
+                    {
+                        var temp = teams[i];
+                        teams[i] = teams[j];
+                        teams[j] = temp;
+                    }
+                }
+            }
         }
     }
 }
